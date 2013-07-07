4fbb0917f7c8be9dd4c562a1d6ba8231e27fbefd
tiankongever <shen10000shen@gmail.com>
2013-07-07 17:19:36 +0800

    20130707171936

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php session_start(); ?>
 <!DOCTYPE HTML>
 <html lang="zh-cn">
@@ -12,15 +13,24 @@
 		<header>
 			<?php include_once('inc/index.top.php'); ?>
 		</header>
-		
+	<div style="float:left;">ส้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้</div>
+	<div style="float:right;">ส็็็็็็็็็็็็็็็็็็็็็็็็็็็็็็็็</div>
 		<section>
 			<?php include_once("inc/index.body.php") ?>	
 		</section>
-		
+	<div style="float:left;">ส้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้</div>
+	<div style="float:right;">ส็็็็็็็็็็็็็็็็็็็็็็็็็็็็็็็็</div>
 		<footer>
 			<?php  include_once('inc/index.footer.php'); ?>
 		</footer>
 		
 		<?php include_once("inc/index.backTop.php") ?>	
+	
+	<div style="float:left;">ส้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้้</div>
+	<div style="float:right;">ส็็็็็็็็็็็็็็็็็็็็็็็็็็็็็็็็</div>
+	<div style="clear:both;"></div>
+	
+
 	</body>
 </html>
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/log/zhizhu.php") ?>	
