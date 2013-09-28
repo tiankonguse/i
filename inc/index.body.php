3ff275092d023415ce4bee9a8c6a2731145310ca
tiankonguse <i@tiankonguse.com>
2013-09-28 21:57:30 +0800

    update

--- inc/index.body.php
+++ inc/index.body.php
@@ -2,29 +2,12 @@
 	<h3>tiankonguse 正在做的事</h3>
 	<ul class="item-list">
 		<li>
-			<a target="_blank" href="/canvas/ex3.html">
-				借别人的东西来过七夕1
+			<a target="_blank" href="/game/sokoban.php">
+				用心打造游戏
 				<span class="new">新</span>
 			</a>
 		</li>		
-		<li>
-			<a target="_blank" href="/canvas/ex5.html">
-				借别人的东西来过七夕2
-				<span class="new">新</span>
-			</a>
-		</li>		
-		<li>
-			<a target="_blank" href="/canvas/ex6.html">
-				借别人的东西来过七夕3
-				<span class="new">新</span>
-			</a>
-		</li>		
-		<li>
-			<a target="_blank" href="/canvas/ex10.html">
-				借别人的东西来过七夕4
-				<span class="new">新</span>
-			</a>
-		</li>		
+		<li>	
 
 		<li>
 			<a target="_blank" href="/rjbq/">
