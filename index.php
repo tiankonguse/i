a10b530e26baa8c79e91cc8b6154a693b3aa59ae
tiankonguse <i@tiankonguse.com>
2013-10-15 15:58:05 +0800

    update

--- index.php
+++ index.php
@@ -1,30 +1,78 @@
-<?php session_start(); ?>
+<?php
+session_start();
+require("./inc/common.php");
+?>
 <!DOCTYPE HTML>
 <html lang="zh-cn">
-	<head>
-		<?php
-		$title = "tiankonguse的世界";
-		include_once('inc/index.header.php');
-		?>
-	</head>
-	
-	<body>
-		<header>
-			<?php include_once('inc/index.top.php'); ?>
-		</header>
+<head>
+<?php
+$title = "tiankonguse' record";
+require BASE_INC . 'head.inc.php';
+?>
+<link href="<?php echo MAIN_DOMAIN;?>css/main.css" rel="stylesheet">
+</head>
 
-		<section>
-			<?php include_once("inc/index.body.php") ?>	
-		</section>
+<body>
+    <header>
+        <div class="title">
+            tiankonguse
+            <script>document.write(new Date().getFullYear());</script>
+        </div>
+    </header>
 
-		<footer>
-			<?php  include_once('inc/index.footer.php'); ?>
-		</footer>
-		
-		<?php include_once("inc/index.backTop.php") ?>	
+    <section>
+        <div class="container">
+            <h3>tiankonguse 正在做的事</h3>
+            <ul class="item-list">
+                <li><a target="_blank" href="/game/sokoban.php"> 用心打造游戏
+                        <span class="new">新</span>
+                </a>
+                </li>
+                <li>
+                
+                <li><a target="_blank" href="/rjbq/"> 人间冰器 </a>
+                </li>
+                <li><a target="_blank" href="/record/"> 记录集 <span
+                        class="new">新</span>
+                </a>
+                </li>
+                <li><a target="_blank" href="/firstblog/"> first blog </a>
+                </li>
+                <li><a target="_blank" href="/messagefk/"> 信息反馈系统 <span
+                        class="new">新</span>
+                </a>
+                </li>
+                <li><a target="_blank" href="/blog/"> 博客 </a>
+                </li>
+                <li><a target="_blank" href="/timeline/"> 生活记录轴 </a>
+                </li>
+                <li><a target="_blank" href="/vatirk/"> acm vatirk团队 </a>
+                </li>
+                <li><a target="_blank" href="/resume/"> 我的简历 </a>
+                </li>
+                <li><a target="_blank" href="/rose/"> 送给女朋友的玫瑰 </a>
+                </li>
+                <li><a target="_blank" href="/earth/"> canvas 画地球 </a>
+                </li>
+                <li><a target="_blank"
+                    href="http://webf.nenu.edu.cn/zwhz/index.php">
+                        中外合作办学网站 </a>
+                </li>
+                <li><a target="_blank"
+                    href="http://acm.nenu.edu.cn/ccr/"> acm校赛注册及比赛管理系统 <span
+                        class="remark">学长主力</span>
+                </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    
+    <script src="<?php echo DOMAIN_JS;?>jquery.js"></script>
+    <footer>
+    <?php  require BASE_INC . 'footer.inc.php'; ?>
+    </footer>
 
-	<div style="clear:both;"></div>
-	
-	</body>
+    <script src="<?php echo DOMAIN_JS;?>main.js"></script>
+
+</body>
 </html>
-<?php include($_SERVER['DOCUMENT_ROOT']."log/zhizhu.php") ?>	
